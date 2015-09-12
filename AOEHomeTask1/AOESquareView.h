f6bfe5c4ba1776e2adaa5657f8ec5b532b4c2fa4
Alina Okhremenko <slykaramba@gmail.com>
2015-09-12 21:13:05 +0300

    empty AOESquareView -> move logic to AOESquareViewController

--- AOEHomeTask1/AOESquareView.h
+++ AOEHomeTask1/AOESquareView.h
@@ -7,20 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-
-typedef NS_ENUM (NSUInteger, AOSquarePosition) {
-    AOSquarePositionLeftTopCorner,
-    AOSquarePositionRigthBottomCorner,
-    AOSquarePositionLeftBottomCorner,
-    AOSquarePositionRigthTopCorner
-};
-
 @interface AOESquareView : UIView
-@property (nonatomic)   AOSquarePosition    squarePosition;
 
-- (void)setSquarePosition:(AOSquarePosition)squarePosition;
-- (void)setSquarePosition:(AOSquarePosition)squarePosition animated:(BOOL)animation;
-- (void)setSquarePosition:(AOSquarePosition)squarePosition
-                 animated:(BOOL)animation
-        completionHandler:(void(^)(void))animationCompletion;
 @end
--- AOEHomeTask1/AOESquareView.m
+++ AOEHomeTask1/AOESquareView.m
@@ -7,52 +7,6 @@
 //
 
 #import "AOESquareView.h"
-
 @implementation AOESquareView
-
-- (void)setSquarePosition:(AOSquarePosition)squarePosition {
-    [self setSquarePosition:squarePosition animated:NO];
-}
-
-- (void)setSquarePosition:(AOSquarePosition)squarePosition
-                 animated:(BOOL)animation
-{
-    [self setSquarePosition:squarePosition animated:YES completionHandler:nil];
-    
-}
-- (void)setSquarePosition:(AOSquarePosition)squarePosition
-                 animated:(BOOL)animation
-        completionHandler:(void (^)(void))animationCompletion
-{
-    if (_squarePosition != squarePosition) {
-        _squarePosition = squarePosition;
-        [UIView animateWithDuration:0.5
-                         animations:^{
-                             switch (self.squarePosition) {
-                                 case AOSquarePositionLeftBottomCorner:
-                                     self.frame = CGRectMake(0, 0, 0, 0);
-                                     break;
-                                 case AOSquarePositionLeftTopCorner:
-                                     self.frame = CGRectMake(0, 0, 200, 100);
-                                     break;
-                                 case AOSquarePositionRigthBottomCorner:
-                                     self.frame =CGRectMake(0, 0, 0, 0);
-                                     break;
-                                 case AOSquarePositionRigthTopCorner:
-                                     self.frame = CGRectMake(0, 0, 0, 0);
-                                     break;
-                             }
-                            }
-                            completion:^(BOOL finished) {
-                                if (animationCompletion != nil) {
-                                    animationCompletion();
-                                }
-                            }
-        ];
-                                                                                                                                  
-    }
-}
-
-                                                                                                                                  
 
 @end
