edac3dc75a12f65d07c599f79cd6b888f82c88f7
Alina Okhremenko <slykaramba@gmail.com>
2015-09-23 00:14:05 +0300

    NSMutableArray + AOEExtensions category added -> method "moveFromIndexToIndex" implemented

---
+++ AOETableViewApp/Sources/Library/Categories/NSMutableArray/NSMutableArray+AOEExtensions.m
@@ -0,0 +1,22 @@
+//
+//  NSMutableArray+AOEExtensions.m
+//  AOETableViewApp
+//
+//  Created by Alina Okhremenko on 23.09.15.
+//  Copyright (c) 2015 Alina Okhremenko. All rights reserved.
+//
+
+#import "NSMutableArray+AOEExtensions.h"
+
+@implementation NSMutableArray (AOEExtensions)
+
+- (void)moveObjectAtIndex:(NSUInteger)fromIndex toIndex:(NSUInteger)toIndex {
+    if (fromIndex < toIndex) {
+        toIndex--;
+    }
+    id object = [self objectAtIndex:fromIndex];
+    [self removeObjectAtIndex:fromIndex];
+    [self insertObject:object atIndex:toIndex];
+}
+
+@end
