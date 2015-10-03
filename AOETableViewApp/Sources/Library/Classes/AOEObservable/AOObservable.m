55f3d06acab4ebae60d05b9403a91becab1cdfd3
Alina Okhremenko <slykaramba@gmail.com>
2015-10-03 11:08:33 +0300

    adds AOObservable class

--- AOETableViewApp/Sources/Library/Classes/AOEObservable/AOObservable.m
+++ AOETableViewApp/Sources/Library/Classes/AOEObservable/AOObservable.m
@@ -1,78 +1,68 @@
-////
-////  AOSubject.m
-////  ObjectiveC
-////
-////  Created by Alina Okhremenko on 03.08.15.
-////  Copyright (c) 2015 Alina Okhremenko. All rights reserved.
-////
 //
-//#import "AOObservable.h"
+//  AOSubject.m
+//  ObjectiveC
 //
-//@interface AOEObservable ()
-//@property (nonatomic, strong)    NSHashTable        *mutableObservers;
+//  Created by Alina Okhremenko on 03.08.15.
+//  Copyright (c) 2015 Alina Okhremenko. All rights reserved.
 //
-//@property (nonatomic, strong)   id<NSLocking>       stateLock;
-//@property (atomic, strong)      dispatch_queue_t    queue;
-//
-//@end
-//
-//
-//@implementation AOEObservable
-//
-//#pragma mark -
-//#pragma mark Deallocation and Initialization Methods
-//
-//- (instancetype)init {
-//    self = [super init];
-//    if (self) {
-//        self.mutableObservers = [NSHashTable weakObjectsHashTable];
-//        self.queue = dispatch_queue_create("observableQueue", DISPATCH_QUEUE_SERIAL);
-//        self.stateLock = [NSLock new];
-//    }
-//    return self;
-//}
-//
-//#pragma mark -
-//#pragma mark - Accessors
-//
-//- (NSArray *)observers {
-//    return [self.mutableObservers allObjects];
-//}
-//
-////- (void)setState:(AOState)state {
-////    [self.stateLock lock];
-////        _state = state;
-////    
-////    [self.stateLock unlock];
-////    
-////    for (id<> observerObject in self.observers) {
-////        [observerObject objectDidChangeState:self];
-////    }
-////}
-//
-////- (AOState)state {
-////    [self.stateLock lock];
-////    
-////     AOState currentState = _state;
-////    
-////    [self.stateLock unlock];
-////    
-////    return currentState;
-////}
-//
-//#pragma mark -
-//#pragma mark  Public Methods
-////
-////- (void)addObserver:(id<>)observer {
-////    @synchronized(self.mutableObservers) {
-////        [self.mutableObservers addObject:observer];
-////    }
-////}
-////
-////- (void)removeObserver:(id<>)observer {
-////    @synchronized(self.mutableObservers) {
-////        [self.mutableObservers removeObject:observer];
-////    }
-////}
-//
-//@end
+
+#import "AOObservable.h"
+
+@interface AOEObservable ()
+@property (nonatomic, strong)    NSHashTable        *mutableObservers;
+
+@end
+
+@implementation AOEObservable
+
+#pragma mark -
+#pragma mark Deallocation and Initialization Methods
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.mutableObservers = [NSHashTable weakObjectsHashTable];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark - Accessors
+
+- (NSArray *)observers {
+    return [self.mutableObservers allObjects];
+}
+
+#pragma mark -
+#pragma mark  Public Methods
+
+- (void)addObserver:(id)observer {
+    [self.mutableObservers addObject:observer];
+}
+- (void)removeObserver:(id)observer {
+    [self.mutableObservers removeObject:observer];
+}
+
+- (BOOL)containsObserver:(id)observer {
+    return [self.mutableObservers containsObject:observer];
+}
+
+- (void)notifyObserversWithSelector:(SEL)selector {
+    [self notifyObserversWithSelector:selector withObject:nil];
+}
+
+- (void)notifyObserversWithSelector:(SEL)selector withObject:(id)object {
+    [self notifyObserversWithSelector:selector withObject:object withObject:nil];
+}
+
+- (void)notifyObserversWithSelector:(SEL)selector withObject:(id)object withObject:(id)object2 {
+    NSArray *observers = [self.mutableObservers allObjects];
+    for (id observer in observers) {
+        if ([observer respondsToSelector:selector]) {
+            [observer performSelector:selector withObject:object withObject:object2];
+        }
+    }
+}
+
+@end
