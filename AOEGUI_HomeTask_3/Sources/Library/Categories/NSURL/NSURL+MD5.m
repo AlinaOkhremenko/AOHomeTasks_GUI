756dbbabd0c20cf2e7a10f4dc10bc218e0446539
Alina Okhremenko <slykaramba@gmail.com>
2015-10-27 01:38:48 +0200

    adds NSUrl + MD5 category

---
+++ AOEGUI_HomeTask_3/Sources/Library/Categories/NSURL/NSURL+MD5.m
@@ -0,0 +1,27 @@
+//
+//  NSURL+MD5.m
+//  AOEGUI_HomeTask_3
+//
+//  Created by Alina Okhremenko on 27.10.15.
+//  Copyright (c) 2015 Alina Okhremenko. All rights reserved.
+//
+
+#import "NSURL+MD5.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSURL (MD5)
+
+- (NSString *)decimalMD5Value {
+    const char *urlString = [[self absoluteString] UTF8String];
+    unsigned char hashBytes[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(urlString,(CC_LONG)strlen(urlString), hashBytes);
+    
+    NSMutableString *hashString = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for ( NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++ ) {
+        [hashString appendFormat:@"%02x", hashBytes[i]];
+    }
+    
+    return hashString;
+}
+
+@end
